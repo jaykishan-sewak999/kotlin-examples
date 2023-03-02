22c6d162c29290f584c0a6c50e87e56710729b9a
jaykishan-sewak <jaykishanbs@infusionanalysts.com>
2023-03-02 18:43:02 +0530

    Added Co-Routine activity for CoRoutine example

---
+++ app/src/main/java/com/example/kotlinexamples/CoroutinExampleActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinexamples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CoroutinExampleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coroutin_example)
+    }
+}
