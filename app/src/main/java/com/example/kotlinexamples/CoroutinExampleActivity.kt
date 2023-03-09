9b7c7f413da56c35c391f231a1c20c56f716f563
jaykishan-sewak <jecky999@gmail.com>
2023-03-09 18:22:58 +0530

    Added co routine functions

--- app/src/main/java/com/example/kotlinexamples/CoroutinExampleActivity.kt
+++ app/src/main/java/com/example/kotlinexamples/CoroutinExampleActivity.kt
@@ -2,11 +2,22 @@ package com.example.kotlinexamples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class CoroutinExampleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coroutin_example)
         // All coroutine code goes here
+    }
+
+    suspend fun function1(): String{
+        Log.i("Launch","Inside function1")
+        return "Thanks for calling function1"
+    }
+
+    suspend fun function2(): String{
+        Log.i("Launch","Inside function2")
+        return "Thanks for calling function2"
     }
 }
