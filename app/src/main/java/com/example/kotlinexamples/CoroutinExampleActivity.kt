c92d975bd462baf6e79f18d5b0c1caa6b045cd34
jaykishan <jecky999@gmail.com>
2023-04-30 20:35:30 +0530

    Added Co-routine functions using launch and async

--- app/src/main/java/com/example/kotlinexamples/CoroutinExampleActivity.kt
+++ app/src/main/java/com/example/kotlinexamples/CoroutinExampleActivity.kt
@@ -2,22 +2,104 @@ package com.example.kotlinexamples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import com.example.kotlinexamples.databinding.ActivityCoroutinExampleBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class CoroutinExampleActivity : AppCompatActivity() {
+
+    private var binding: ActivityCoroutinExampleBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_coroutin_example)
-        // All coroutine code goes here
+        binding = ActivityCoroutinExampleBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        binding?.btnCallCoRoutine?.setOnClickListener {
+            coRoutineUsingLaunch()
+            coRoutineUsingAsync()
+        }
     }
 
-    suspend fun function1(): String{
-        Log.i("Launch","Inside function1")
-        return "Thanks for calling function1"
+    private fun coRoutineUsingLaunch(){
+        val desc = StringBuilder()
+        desc.append("Main function(out of coroutine) Started \n")
+        GlobalScope.launch {
+            val funAResult = printFunctionsALaunch(desc)
+            val funBResult = printFunctionsBLaunch(desc)
+            // below code will run one by once line as is in launch
+            desc.append(funAResult)
+            printDescA(desc)
+            desc.append(funBResult)
+            printDescA(desc)
+            desc.append("\nFunc A and Call finished Concat:$funAResult$funBResult")
+            printDescA(desc)
+        }
+        desc.append("\n Main function(out of coroutine) End")
     }
 
-    suspend fun function2(): String{
-        Log.i("Launch","Inside function2")
-        return "Thanks for calling function2"
+    private fun coRoutineUsingAsync(){
+        val desc = StringBuilder()
+        desc.append("Main function(out of coroutine) Started \n")
+        GlobalScope.launch {
+            val funAResult = async {printFunctionsAAsync(desc)}
+            val funBResult = async { printFunctionsBAsync(desc) }
+            // below code will run one by once line as is in launch
+            desc.append(funAResult.await())
+            printDescB(desc)
+            desc.append(funBResult.await())
+            printDescB(desc)
+            desc.append("\nFunc A and Call finished Concat:${funAResult.await()}${funBResult.await()}")
+            printDescB(desc)
+        }
+        desc.append("\n Main function(out of coroutine) End")
+    }
+
+    private fun printDescA(desc: StringBuilder) {
+        runOnUiThread {
+            binding?.tvLaunchOp?.text = desc
+        }
+    }
+
+    private fun printDescB(desc: StringBuilder) {
+        runOnUiThread {
+            binding?.tvAsyncOp?.text = desc
+        }
+    }
+
+    private suspend fun printFunctionsALaunch(desc: StringBuilder): String {
+        desc.append("\n Function A started")
+        printDescA(desc)
+        delay(3000)
+        desc.append("\n Function A End - after delay")
+        printDescA(desc)
+        return ("\n Function A finished")
+    }
+
+    private suspend fun printFunctionsBLaunch(desc: StringBuilder): String {
+        desc.append("\n Function B started")
+        printDescA(desc)
+        delay(100)
+        desc.append("\n Function B End - after delay") // in output you will notice that this function called after when its called from async
+        printDescA(desc)
+        return "\n Function b finished"
+    }
+
+    private suspend fun printFunctionsAAsync(desc: StringBuilder): String {
+        desc.append("\n Function A started")
+        printDescA(desc)
+        delay(3000)
+        desc.append("\n Function A End - after delay")
+        printDescA(desc)
+        return ("\n Function A finished")
+    }
+
+    private suspend fun printFunctionsBAsync(desc: StringBuilder): String {
+        desc.append("\n Function B started")
+        printDescA(desc)
+        delay(100)
+        desc.append("\n Function B End - after delay") // in output you will notice that this function called first when its called from async
+        printDescA(desc)
+        return "\n Function b finished"
     }
 }
